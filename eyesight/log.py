ca54d423ff7fcab13ed930d1677b8ef98dbd52ac
lojoja <github@lojoja.com>
2017-07-02 15:22:58 -0400

    Fix log handler order
    Prevents color escape codes being printed in file log

--- eyesight/log.py
+++ eyesight/log.py
@@ -17,13 +17,13 @@ def get_level(verbose):
 
 
 def init(logger, verbose):
-    click_handler = ClickHandler()
-    click_handler.setLevel(get_level(verbose))
-    click_handler.setFormatter(ClickFormatter())
-    logger.addHandler(click_handler)
-
     if IS_ROOT:
         file_handler = RotatingFileHandler(FILE, maxBytes=10485760, backupCount=5, encoding='utf8')
         file_handler.setLevel(logging.DEBUG)
         file_handler.setFormatter(logging.Formatter(RECORD_FORMAT, DATE_FORMAT))
         logger.addHandler(file_handler)
+
+    click_handler = ClickHandler()
+    click_handler.setLevel(get_level(verbose))
+    click_handler.setFormatter(ClickFormatter())
+    logger.addHandler(click_handler)
